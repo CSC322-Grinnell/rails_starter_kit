bc5ab6540b3cd6c29a9e5bc95e8907f79118a928
Ian Young <ian@iangreenleaf.com>
2018-07-11 22:51:40 -0500

    Restrict admin interface to only some users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def authenticate_admin_user!
+    unless current_user && current_user.admin?
+      reset_session
+      redirect_to(new_user_session_path)
+    end
+  end
 end
